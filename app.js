90cfd95c27a238f2801f8142af90f6b4fb28f54c
Sal Tamay <sal.tamay@outlook.com>
2020-04-10 13:36:08 -0400

    feat: Add attack feature

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ new Vue({
     showGameButtons: false,
     showGameLog: false,
     showStartGame: true,
+    playerHealth: 100,
+    monsterHealth: 100,
   },
   methods: {
+    attack: function () {
+      const playerDamage = Math.ceil(Math.random() * 10);
+      const monsterDamage = Math.ceil(Math.random() * 10);
+
+      console.log(playerDamage);
+      console.log(monsterDamage);
+
+      this.playerHealth -= playerDamage;
+      this.monsterHealth -= monsterDamage;
+    },
     startGame: function () {
       this.showStartGame = false;
       this.showGameButtons = true;
