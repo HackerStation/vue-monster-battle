933e3fcec214390d881539b76aede2b4f12fb9d0
Sal Tamay <sal.tamay@outlook.com>
2020-04-10 14:30:11 -0400

    feat: Add special attack option

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     hits: [],
+    numOfSpecialAttacks: 3,
   },
   methods: {
-    attack: function () {
+    attack: function (e, playerAttackLevel = 10) {
       this.showGameLog = true;
 
-      const playerDamage = Math.ceil(Math.random() * 10);
+      const playerDamage = Math.ceil(Math.random() * playerAttackLevel);
       const monsterDamage = Math.ceil(Math.random() * 10);
 
       this.hits.push({
@@ -22,6 +23,22 @@ new Vue({
 
       this.playerHealth -= playerDamage;
       this.monsterHealth -= monsterDamage;
+    },
+    specialAttack: function name() {
+      if (this.numOfSpecialAttacks > 0) {
+        this.showGameLog = true;
+
+        const playerDamage = Math.ceil(Math.random() * 10);
+        const monsterDamage = Math.ceil(Math.random() * 20);
+
+        this.hits.push({
+          player: playerDamage,
+          monster: monsterDamage,
+        });
+
+        this.playerHealth -= playerDamage;
+        this.monsterHealth -= monsterDamage;
+      }
     },
     startGame: function () {
       this.showStartGame = false;
